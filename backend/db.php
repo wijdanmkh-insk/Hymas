b5912c567c60c248be9bd65448a39008b1bcc69b
TakanashaTaryu <tatsuarieyu@gmail.com>
2025-01-17 20:48:26 +0700

    modified:   backend/db.php

--- backend/db.php
+++ backend/db.php
@@ -7,6 +7,7 @@ $pass = '((.I3Q5$uA9~';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
