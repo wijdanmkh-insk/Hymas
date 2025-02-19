a65608089a1031520dca44c0975eedd15a5c8e20
TakanashaTaryu <tatsuarieyu@gmail.com>
2025-02-19 21:58:55 +0700

    modified:   backend/db.php

--- backend/db.php
+++ backend/db.php
@@ -15,7 +15,6 @@ $user = $_ENV['DB_USERNAME'];
 $pass = $_ENV['DB_PASSWORD'];
 
 try {
-    $pdo = new PDO("mysql:host=localhost;port=3306;dbname=hymas", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected to the database successfully!";
 } catch (PDOException $e) {
